1105ffa1ae60668952c7eb637ed456a6e4e073f6
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-04 02:13:36 +0330

    update

--- learningBlockChain/src/Hash/SHA256Helper.java
+++ learningBlockChain/src/Hash/SHA256Helper.java
@@ -7,6 +7,10 @@ public class SHA256Helper {
     public static String hash(String data){
         try{
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(data.getBytes("UTF-8"));
+
+            // we want to end up with hexadecimal values not bytes
+            StringBuffer hexadecimalString = new StringBuffer();
         }catch(Exception e){
             throw new RuntimeException(e);
         }
