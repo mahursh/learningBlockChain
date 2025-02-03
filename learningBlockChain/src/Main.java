de5b23cfbdd40ddb8e07535aea170825233ead1d
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-04 02:24:29 +0330

    update

--- learningBlockChain/src/Main.java
+++ learningBlockChain/src/Main.java
@@ -1,5 +1,7 @@
+import Hash.SHA256Helper;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(SHA256Helper.hash("Hello World"));
     }
 }
