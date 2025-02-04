6f6970d6abbc468d8d3628d2257fb51f3284e710
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-04 18:05:01 +0330

    update

--- learningBlockChain/src/SimpleBlockchainFromScratch/Constants.java
+++ learningBlockChain/src/SimpleBlockchainFromScratch/Constants.java
@@ -1,4 +1,18 @@
 package SimpleBlockchainFromScratch;
 
 public class Constants {
+
+    private Constants() {
+
+    }
+    public static final int DIFFICULTY = 1;
+
+    //This is the reward in the underlying cryptocurrency.
+    public static final int REWARD = 10;
+
+    //First block (genesis)
+    public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
+
+
+
 }
