ea92ee3883a5019eb622f634e513f2a19e1990cd
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-04 17:52:47 +0330

    update

--- learningBlockChain/src/Main.java
+++ learningBlockChain/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(SHA256Helper.hash("Hello World"));
 
-        //Double Hashing -> what BlockChain is using under hood
+        //Double Hashing -> what BlockChain is using under the hood
         System.out.println(SHA256Helper.hash(SHA256Helper.hash("Hello World")));
 
     }
