4d484c08e2ec01346269377ba418d929f5a5ba3a
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-04 23:18:37 +0330

    update

---
+++ learningBlockChain/src/Hash/App.java
@@ -0,0 +1,15 @@
+package Hash;
+
+public class App {
+    public static void main(String[] args) {
+//        System.out.println(SHA256Helper.hash("Hello World"));
+
+        //Double Hashing -> what BlockChain is using under the hood
+//        System.out.println(SHA256Helper.hash(SHA256Helper.hash("Hello World")));
+
+
+        for(Integer i = 0; i<100; i++)
+            System.out.println(SHA256Helper.hash(i.toString()));
+
+    }
+}
--- learningBlockChain/src/Main.java
+++ learningBlockChain/src/Main.java
@@ -1,15 +1,8 @@
-import Hash.SHA256Helper;
+
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(SHA256Helper.hash("Hello World"));
-
-        //Double Hashing -> what BlockChain is using under the hood
-//        System.out.println(SHA256Helper.hash(SHA256Helper.hash("Hello World")));
-
-
-        for(Integer i = 0; i<100; i++)
-            System.out.println(SHA256Helper.hash(i.toString()));
 
     }
+
 }
