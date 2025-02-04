cf633067c9d3b26d81796e3224411b20de5048ad
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-04 19:46:28 +0330

    update

--- learningBlockChain/src/SimpleBlockchainFromScratch/Miner.java
+++ learningBlockChain/src/SimpleBlockchainFromScratch/Miner.java
@@ -1,4 +1,31 @@
 package SimpleBlockchainFromScratch;
 
 public class Miner {
+
+    private double reward;
+
+    public void mine(Block block, BlockChain blockChain) {
+
+        // it takes some time to find the valid hash
+        // PoW (Prof of work)
+        while (!isGoldenHash(block)) {
+            block.incrementNonce();
+            block.generateHash();
+        }
+        System.out.println(block + "has been mined ...");
+        System.out.println("Hash is : " + block.getHash());
+
+        blockChain.addBlock(block);
+        reward += Constants.REWARD;
+    }
+
+    private boolean isGoldenHash(Block block) {
+        String leadingZeros = new String(new char[Constants.DIFFICULTY]).replace('\0', '0');
+        return block.getHash().substring(0, Constants.DIFFICULTY).equals(leadingZeros);
+    }
+
+
+    public double getReward() {
+        return this.reward;
+    }
 }
