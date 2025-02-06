d5e455bc62b4eed6af4d79270b5322e1f2ea385c
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-06 03:37:41 +0330

    update

--- learningBlockChain/src/Cryptocurency/App.java
+++ learningBlockChain/src/Cryptocurency/App.java
@@ -1,4 +1,13 @@
 package Cryptocurency;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Security;
+
 public class App {
+    public static void main(String[] args) {
+
+        // we use bouncy castle as the cryptography related provider
+        Security.addProvider(new BouncyCastleProvider());
+    }
 }
