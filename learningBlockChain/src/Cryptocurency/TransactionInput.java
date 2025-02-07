2853f255b8751194e557e9b93c39cbb294f9efb1
Mahoor.Sh <mahurshams94@gmail.com>
2025-02-07 21:02:53 +0330

    update

--- learningBlockChain/src/Cryptocurency/TransactionInput.java
+++ learningBlockChain/src/Cryptocurency/TransactionInput.java
@@ -1,4 +1,30 @@
 package Cryptocurency;
 
 public class TransactionInput {
+
+    // every input has an output. this id is the transactionId of the TransactionOutput
+    private String transactionOutputId;
+
+    // this is the unspent transaction output
+    private TransactionOutput UTXO;
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+    public String getTransactionOutputId() {
+        return transactionOutputId;
+    }
+    public TransactionInput setTransactionOutputId(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+        return this;
+    }
+
+    public TransactionOutput getUTXO() {
+        return UTXO;
+    }
+    public TransactionInput setUTXO(TransactionOutput UTXO) {
+        this.UTXO = UTXO;
+        return this;
+    }
+
 }
